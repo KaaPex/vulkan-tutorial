4829bee73735bff833f104df8406781f47db941a
Aleksei Kazantsev <develop.kaa@gmail.com>
2024-07-07 11:06:45 +0300

    Chapter 6  - create instance

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,22 @@
     clippy::unnecessary_wraps
 )]
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use log::*;
+
 use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
+
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
+use vulkanalia::Version;
+
+/// The Vulkan SDK version that started requiring the portability subset extension for macOS.
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
@@ -63,11 +73,19 @@ fn main() -> Result<()> {
 #[derive(Default)]
 struct App {
     window: Option<Window>,
+    pub entry: Option<Entry>,
+    pub instance: Option<Instance>,
 }
 
 impl App {
     /// Creates our Vulkan app.
     unsafe fn create(&mut self) -> Result<()> {
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        self.entry = Some(Entry::new(loader).map_err(|b| anyhow!("{}", b)).unwrap());
+        self.instance = Some(create_instance(
+            self.window.as_ref().unwrap(),
+            &self.entry.as_ref().unwrap(),
+        )?);
         Ok(())
     }
 
@@ -77,9 +95,52 @@ impl App {
     }
 
     /// Destroys our Vulkan app.
-    unsafe fn destroy(&mut self) {}
+    unsafe fn destroy(&mut self) {
+        self.instance.as_ref().unwrap().destroy_instance(None);
+    }
 }
 
 /// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
 struct AppData {}
+
+//================================================
+// Instance
+//================================================
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    // Application Info
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    // Extensions
+    let mut extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    // Required by Vulkan SDK on macOS since 1.3.216.
+    let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+        info!("Enabling extensions for macOS portability.");
+        extensions.push(
+            vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION
+                .name
+                .as_ptr(),
+        );
+        extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::empty()
+    };
+
+    // Create
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions);
+
+    Ok(entry.create_instance(&info, None)?)
+}
